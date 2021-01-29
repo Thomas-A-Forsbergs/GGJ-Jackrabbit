8b276d86ebc0406a0625b8788e35407b4d32ff6b
Jackoberto <jackoberto01@gmail.com>
2021-01-29 21:45:07 +0100

    Starting some basic movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Transform cameraTransform, forwardTransform;
+    public KeyCode moveKey = KeyCode.Space;
+    public float moveSpeed;
+
+    public void Update()
+    {
+        if (Input.GetKey(moveKey))
+        {
+            MoveInDirectionCameraIsPointing();
+        }
+    }
+
+    private void MoveInDirectionCameraIsPointing()
+    {
+        var forward = cameraTransform.forward;
+        var dir = (Vector3.forward - cameraTransform.localPosition).normalized;
+        transform.Translate(new Vector3(dir.x * Time.deltaTime * moveSpeed, 0, dir.z * Time.deltaTime * moveSpeed));
+    }
+}
