f0e6377c33c70b93b6f78c3882d0779ec58df81d
Linus Å <71346418+LynkanRex@users.noreply.github.com>
2021-01-29 21:23:31 +0100

    Adds IMessageHandler.cs, WinLoseConditionsBroker.cs, Timer.cs, basic LoseCondition.cs

---
+++ Assets/Scripts/LoseCondition.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class LoseCondition : MonoBehaviour
+{
+    private WinLoseConditionsBroker _eventHandler;
+    
+    void Start()
+    {
+        _eventHandler = FindObjectOfType<WinLoseConditionsBroker>();
+        _eventHandler.SubscribeTo<string>(GameOver);
+    }
+
+    void GameOver(string argument)
+    {
+        Debug.Log("Event" + argument + "fired");
+        _eventHandler.UnsubscribeFrom<string>(GameOver);
+        Time.timeScale = 0;
+    }
+}
---
+++ Assets/Scripts/Timer/Timer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private float gameTimerInSeconds;
+    private float timerInterval;
+    private float elapsedTime;
+    
+    private bool TimerOut => gameTimerInSeconds <= 0;
+    
+    private WinLoseConditionsBroker _eventHandler;
+    
+    void Start()
+    {
+        _eventHandler = FindObjectOfType<WinLoseConditionsBroker>();
+        timerInterval = 1.0f;
+    }
+    
+    void Update()
+    {
+        if (TimerOut) TimeOut();
+        elapsedTime = elapsedTime + Time.deltaTime;
+        
+        if (elapsedTime >= timerInterval)
+        {
+            gameTimerInSeconds -= timerInterval;
+            elapsedTime -= timerInterval;
+        }
+    }
+
+    void TimeOut()
+    {
+        _eventHandler.Publish("TimeOut");
+    }
+}
