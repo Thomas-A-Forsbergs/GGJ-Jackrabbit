d5eeb9d03b8d1c7d36684a5879ae6cd77d686bd5
Jackoberto <jackoberto01@gmail.com>
2021-01-29 23:35:34 +0100

    Fixed Player sprite

---
+++ Assets/MatchTransform.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+public class MatchTransform : MonoBehaviour
+{
+    public Transform transformToFollow;
+
+    private void Update()
+    {
+        transform.position = transformToFollow.position;
+    }
+}
