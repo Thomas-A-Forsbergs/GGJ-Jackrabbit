01f336e3f2d43002009f4fbf32ef965c19ca3618
Jackoberto <jackoberto01@gmail.com>
2021-01-30 00:07:02 +0100

    Made player follow correctly

---
+++ Assets/Scripts/PlayerLookAtCamera.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class PlayerLookAtCamera : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, Camera.main.transform.eulerAngles.y, Camera.main.transform.eulerAngles.z);
+    }
+}
