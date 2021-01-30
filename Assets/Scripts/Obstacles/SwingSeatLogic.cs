2253f4d5411441e2d2bf4b9b9563a918181b0d50
Linus Å <71346418+LynkanRex@users.noreply.github.com>
2021-01-30 19:43:09 +0100

    Adds SwingSeatLogic which locks the Players IsStuck until it leaves

---
+++ Assets/Scripts/Obstacles/SwingSeatLogic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwingSeatLogic : MonoBehaviour
+{
+    private PlayerMovement playerRef;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerRef = other.gameObject.GetComponent<PlayerMovement>();
+            playerRef.isStuck = true;
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (playerRef != null)
+        {
+            playerRef.isStuck = false;
+            playerRef = null;
+        }
+    }
+}
