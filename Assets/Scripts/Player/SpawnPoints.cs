b956592b2279dd7d8e6c7e5570528dac4e716c55
Xiaodi Wang <xiaodi.wang@student.forsbergsskola.se>
2021-01-30 22:02:08 +0100

    Add SpawnPoint Script and Prefab

---
+++ Assets/Scripts/Player/SpawnPoints.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoints : MonoBehaviour {
+    private GameObject player;
+    private List<Transform> spawnPoints;
+
+    private void Start() {
+        spawnPoints = new List<Transform>();
+        player = GameObject.FindWithTag("Player");
+        var children = GetComponentInChildren<Transform>();
+        foreach(Transform child in children) {
+            spawnPoints.Add(child.transform);
+        }
+        int randomIndex = UnityEngine.Random.Range(0, spawnPoints.Count-1);
+        player.transform.position = spawnPoints[randomIndex].position;
+    }
+}
