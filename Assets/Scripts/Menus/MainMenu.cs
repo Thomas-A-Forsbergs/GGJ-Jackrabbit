ce7d3d04ceb4b2f4bd096eb778de3da7ea6fd0f2
Linus Å <71346418+LynkanRex@users.noreply.github.com>
2021-01-30 10:46:13 +0100

    Adds Main Menu Prefab + scripts, Introduces MainScene to buildOrder

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGameButton()
+    {
+        //Loads the next scene in the build order
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+    
+    public void QuitButton()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
+    }
+}
