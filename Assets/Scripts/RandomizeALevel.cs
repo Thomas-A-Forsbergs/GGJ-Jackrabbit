db731a7f74a42b550df304bc2b81b352f47bc137
Jackoberto <jackoberto01@gmail.com>
2021-01-31 14:01:59 +0100

    Fixes UI and start on randomize levels

---
+++ Assets/Scripts/RandomizeALevel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RandomizeALevel : MonoBehaviour
+{
+    public List<GameObject> levelPrefabs;
+
+    private void Awake()
+    {
+        var random = Random.Range(0, levelPrefabs.Count);
+        Instantiate(levelPrefabs[random]);
+    }
+}
